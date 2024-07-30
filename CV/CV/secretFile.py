d8293d0032d1773c21184e24a9083509526eb0ad
Dana Karimi <danak1382@gmail.com>
2024-07-30 19:43:29 +0330

    first commit

---
+++ CV/CV/secretFile.py
@@ -0,0 +1,5 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-&dvwrlu(so%1^rx$n+bp0rax+05qb3ou6t57dyzww5zei979)@'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
